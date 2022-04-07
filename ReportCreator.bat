64b92e228b94fea0ac418ff8fd8f96fbd4a86cd7
MoisesOR <37635283+MoisesOR@users.noreply.github.com>
2022-04-07 18:15:32 +0200

    Update ReportCreator.bat

--- ReportCreator.bat
+++ ReportCreator.bat
@@ -2,14 +2,13 @@
 echo Set the parameters:
 echo(
 
-echo ***** SET INTO "" *****
 set /p title=Title:
-IF NOT DEFINED title SET "title=Jmeter Report" && echo Setting default title...
+if not defined title set title=QA Cross Jmeter Report && echo Setting default title...
 set /p csv=CSV path: 
 set /p folder=Folder path (It must be empty):
 echo ***** IF GRANULARITY IS NULL, IT SETS TO 60000 MS *****
 set /p ms=Granularity (ms):
-IF NOT DEFINED ms SET "ms=60000" && echo Setting default granularity...
+if not defined ms set ms=60000 && echo Setting default granularity...
 echo(
 
 echo Title: %title%
@@ -18,6 +17,6 @@ echo Folder: %folder%
 echo Granularity: %ms%
 echo(
 
-set /p ready = Click to create report.
+set /p ready=Click to create report.
 
 jmeter -Jjmeter.reportgenerator.overall_granularity=%ms% -Jjmeter.reportgenerator.report_title="%title%" -g %csv% -o %folder%
